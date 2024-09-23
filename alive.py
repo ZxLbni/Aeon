411dd21b21c4ead75be6a4f053e53083d620bdd7
Noob <101978891+ZxLbni@users.noreply.github.com>
2024-09-23 20:21:50 +0530

    Add files via upload

--- alive.py
+++ alive.py
@@ -1,33 +1,22 @@
 import os
 import time
 import logging
-
 import requests
 
-BASE_URL = os.environ.get("BASE_URL", None)
-
+BASE_URL = os.environ.get('BASE_URL', None)
 try:
-    if not BASE_URL:
+    if len(BASE_URL) == 0:
         raise TypeError
     BASE_URL = BASE_URL.rstrip("/")
 except TypeError:
     BASE_URL = None
-
-PORT = os.environ.get("PORT", None)
-
-
-def check_status():
-    try:
-        requests.get(BASE_URL).status_code
-    except Exception as e:
-        logging.error(f"alive.py: {e}")
-        return False
-    return True
-
-
-if PORT and BASE_URL:
+PORT = os.environ.get('PORT', None)
+if PORT is not None and BASE_URL is not None:
     while True:
-        if check_status():
+        try:
+            requests.get(BASE_URL).status_code
             time.sleep(400)
-        else:
+        except Exception as e:
+            logging.error(f"alive.py: {e}")
             time.sleep(2)
+            continue
